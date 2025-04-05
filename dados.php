6c0d3b872202fa87bef09fcabe2840193f5df3ac
Gustavo Henrique <gustavohds2019@outlook.com>
2025-04-04 23:38:54 -0300

    começo

---
+++ dados.php
@@ -0,0 +1,41 @@
+<?php
+
+    $personagens = [
+        [
+            "id" => 1,
+            "nome" => "Goku",
+            "imagem" => "#",
+            "descricao" => "#",
+            "categoria" => "Shounen"
+        ],
+        [
+            "id" => 2,
+            "nome" => "Goku",
+            "imagem" => "#",
+            "descricao" => "#",
+            "categoria" => "Shounen"
+        ],
+        [
+            "id" => 3,
+            "nome" => "Goku",
+            "imagem" => "#",
+            "descricao" => "#",
+            "categoria" => "Shounen"
+        ],
+        [
+            "id" => 4,
+            "nome" => "Goku",
+            "imagem" => "#",
+            "descricao" => "#",
+            "categoria" => "Shounen"
+        ],
+        [
+            "id" => 5,
+            "nome" => "Goku",
+            "imagem" => "#",
+            "descricao" => "#",
+            "categoria" => "Shounen"
+        ],
+    ];
+
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include 'dados.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catálogo de Personagens</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .card-img-top {
+            width: 100%;
+            height: 250px;
+            object-fit: cover;
+        }
+    </style>
+</head>
+<body class="bg-dark text-white">
+
+        <div class="container mt-5">
+            <h1></h1>
+
+        </div>
+    
+</body>
+</html>
